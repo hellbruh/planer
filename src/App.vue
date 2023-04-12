db54a2d73ccc18bec0fabc03bcc013f0b5940d43
Annya <annya@example.com>
2023-04-12 20:10:01 +0500

    1204 temik chto-to zadal

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,76 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div v-if="folders.length===0">
+      <p id="nothing">Задач нет</p>
+    </div>
+    <div id="folders" v-for="(folder) in folders"
+    :key=folder.id>
+      <div>
+        <button class="folderButton"></button>
+      </div>
+      <button id="createFolderButton" @click="addFolder()">Create</button>
+      <div v-if="visibleForm===1">
+        <new-folder></new-folder>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+// import HelloWorld from './components/HelloWorld.vue'
+import NewFolder from './components/NewFolder.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    NewFolder
+  },
+  data(){
+    return{
+      folders:[
+        // {
+        //   id: 1,
+        //   name: 'work',
+        //   color: '#FFF0F5'
+        // },
+        // {
+        //   id:2,
+        //   name:'weee',
+        //   color:'#FAEBD7'
+        // }
+
+      ],
+      visibleForm:0,
+
+    }
+  },
+  methods:{
+    addFolder(){
+      this.visibleForm = 1
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+#app{
+  display:flex;
+  flex-direction:column;
+  margin:0%;
 }
+#folders{
+    display: flex;
+    flex-direction:column;
+    justify-content: center;
+    text-align:justify;
+ }
+ #nothing{
+  font-family: 'Sofia Sans', sans-serif;
+  font-size:28px;
+ }
+ #createFolderButton{
+  width:100px;
+  height:100px;
+  display:flex;
+ }
+ 
 </style>
---
+++ src/components/NewFolder.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    
+  </div>
+</template>
+
+<script>
+export default {
+name:'NewFolder',
+data(){
+    return{
+        newFolder:{
+            name:'t',
+            color:'er'
+        }
+    }
+}
+}
+</script>
+
+<style>
+
+</style>
