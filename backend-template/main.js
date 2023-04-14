9dc25e0b0c4a2f26a979363da575f2b7c4e5a954
Annya <annya@example.com>
2023-04-14 20:03:45 +0500

    140423 postgres

---
+++ backend-template/main.js
@@ -0,0 +1,90 @@
+// Импортируем библиотеку fastify для развертывания веб-сервера
+const fastify = require('fastify')({
+    logger: true // Эта штука нужна, чтобы в терминале отображались логи запросов
+})
+const Pool = require('pg-pool')
+const pool = new Pool({
+    database: 'postgres',
+    user: 'postgres',
+    password: '123456789',
+    ssl: false,
+    port: 5432,
+    max: 20, // set pool max size to 20
+    idleTimeoutMillis: 1000, // close idle clients after 1 second
+    connectionTimeoutMillis: 1000, // return an error after 1 second if connection could not be established
+    maxUses: 7500, // close (and replace) a connection after it has been used 7500 times (see below for discussion)
+  })
+//   передавать через енв
+
+pool.on('error', (error, client) => {
+    console.error(error)
+    process.exit(-1)
+})
+pool.on('connect', client => {
+    console.log('New client')
+})
+pool.on('remove', client => {
+    console.log('Client pool removed')
+})
+
+// Блок кода, который нужен для исправления ошибки с CORS
+fastify.register(require('@fastify/cors'), (instance) => {
+    return (req, callback) => {
+        const corsOptions = {
+            // This is NOT recommended for production as it enables reflection exploits
+            origin: true
+        };
+
+        // do not include CORS headers for requests from localhost
+        if (/^localhost$/m.test(req.headers.origin)) {
+            corsOptions.origin = false
+        }
+
+        // callback expects two parameters: error and options
+        callback(null, corsOptions)
+    }
+})
+
+fastify.get('/', async function(requesr,reply) {
+    const client = await pool.connect()
+    // непосредственное подкллючение к бд
+
+    try{
+        const users = await client.query('select * from users')
+        console.log(users)
+    }
+    catch(e){
+        console.log(e)
+    }
+    finally{
+        client.release()
+    }
+})
+
+
+
+// Создание маршрута для post запроса
+fastify.post('/post',function (request, reply) {
+    console.log(`Тело запроса: `,JSON.stringify(request.body))
+    reply.send(request.body)
+})
+
+// Создание запроса с использование path параметров
+fastify.get('/:id',function (request, reply) {
+    console.log(`Path параметры, переданные в запросе: `,JSON.stringify(request.params))
+    reply.send(request.params)
+})
+
+// Создание запроса с использованием query параметров
+fastify.get('/query',function (request, reply) {
+    console.log(`Query параметры, переданные в запросе`, JSON.stringify(request.query))
+    reply.send(request.query)
+})
+
+// Запускаем сервер на порту 3000
+fastify.listen({ port: 3000 }, function (err, address) {
+    if (err) {
+        fastify.log.error(err)
+        process.exit(1)
+    }
+})
